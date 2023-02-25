be480fc996cde10d13a1291ce5ad277bbd4bead5
Patrick Kneschke <patrick.kneschke@gmx.de>
2023-02-25 15:36:07 +0100

    Cleanup and comments.

--- event_system.h
+++ event_system.h
@@ -24,6 +24,12 @@ struct Event {
 };
 
 
+/* @brief Singleton class that maps EventTypes to a list of EventListeners.
+ *        Events can be submitted for immediate publication or stored in a queue to be published by calling Update().
+ *        EventSystem instance is initialized by calling StartUp(), before events can be submitted. 
+ *        Eventsystem instance is destroyed by calling ShutDown(), after all EventListeners have been destroyed. 
+ * @class
+ */
 struct EventListener;
 
 class EventSystem {
@@ -32,12 +38,24 @@ class EventSystem {
 
 public:
 
+    /* @brief Create new EventSystem instance if not already initialized.
+    */
     static void StartUp();
+
+    /* @brief Destroys EventSystem instance if already initialized.
+    */
     static void ShutDown();
 
+    /* @brief Submits an Event for future publication when Update() is called.
+    */
     static void PublishQueued(const Event &event);
+
+    /* @brief Submits an Event to be published now.
+    */
     static void PublishImmediate(const Event &event);
 
+    /* @brief Publishes all Events currently stored in the event queue.
+    */
     static void Update();
 
     ~EventSystem() = default;
@@ -45,8 +63,20 @@ public:
 
 private:
 
+    /* @brief Returns a new unique id for an EventListener.
+    */
     static EventListenerID GetNewListenerID();
+
+    /* @brief Adds an EventListener to the Eventsystem by mapping it to the EventTypes it responds to.
+     *
+     * @param listener    The EventListener to add to the EventSystem,
+    */
     static void Subscribe(const EventListener *listener);
+
+    /*@brief Removes the EventListener from the EventSystem.
+     *
+     * @param listener    The EventListener to remove from the EventSystem,
+    */
     static void Unsubscribe(const EventListener *listener);
 
     EventSystem();
@@ -60,6 +90,12 @@ private:
 };
 
 
+/* @brief Wrapper for an EventCallback that also stores a list of EventTypes the callback responds to. 
+ *        Recieves a unique id from the EventSystem upon initialization.
+ *        Subscribes to the EventSystem on initialization and unsubscribes on destruction.     
+ *
+ * @struct
+ */
 struct EventListener {
 
     EventListener(EventCallback &&callback_, std::vector<EventType> &&types_) 
